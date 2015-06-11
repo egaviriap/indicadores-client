cb45060b2f373d014ae6b7e9de4e066b5c63d709
unknown <egaviria@CHCEMEBW03XR11.corp.choucairtesting.local>
2015-06-11 10:37:08 -0500

    Ip

--- public/ind/assets/globals/config.js
+++ public/ind/assets/globals/config.js
@@ -2,6 +2,6 @@
  * Created by egaviria on 15/05/2015.
  */
 var config = {
-    //apiEndpoint: "http://www.choucairtesting.com:4000/api/"
-    apiEndpoint: "http://172.17.45.49:4000/api/"
+    apiEndpoint: "http://www.choucairtesting.com:4000/api/"
+   // apiEndpoint: "http://172.17.45.49:4000/api/"
 };
